c116e259d9d7c9605ba57570fef8d0fca37feece
ti-jumbo <jumbo.ti@jumboalimentos.com.br>
2026-04-03 17:17:05 -0300

    release v0.0.2

--- src/main/java/com/sysnormal/security/auth/sso/starter/sysnormal_spring_boot_starter_sso/configs/JwtAutoConfiguration.java
+++ src/main/java/com/sysnormal/security/auth/sso/starter/sysnormal_spring_boot_starter_sso/configs/JwtAutoConfiguration.java
@@ -4,9 +4,11 @@ import com.sysnormal.security.auth.sso.starter.sysnormal_spring_boot_starter_sso
 import com.sysnormal.security.auth.sso.starter.sysnormal_spring_boot_starter_sso.services.jwt.JwtSsoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
@@ -28,8 +30,8 @@ public class JwtAutoConfiguration {
     * {@code @important} This bean must be defined via @Bean (not @Service)
     * to avoid multiple candidates when extending JwtService (e.g. JwtSsoService).
      */
-    @Bean
-    //@ConditionalOnMissingBean({JwtSsoService.class})
+    @Bean(name = "jwtSsoService")
+    @ConditionalOnMissingBean(name = "jwtSsoService")
     public JwtSsoService jwtSsoService(JwtSsoProperties jwtSsoProperties) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
         return new JwtSsoService(jwtSsoProperties);
     }
